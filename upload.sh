21673581e5ae2d4a5254848a594010f34cb7a3df
Sam Havens <sam@mosaicml.com>
2022-12-20 17:41:15 +0000

    upload

--- upload.sh
+++ upload.sh
@@ -4,11 +4,16 @@ if ! [ -x "$(command -v aws)" ]; then
   exit 1
 fi
 
+# Make sure there is an s3 bucket passed
+if (( $# != 1 )); then
+    >&2 echo "This script takes one argument; the s3 bucket URI"
+fi
+
 # Set the path to the directory to upload
 dir_path=./mds-pol
 
 # Set the S3 bucket URL
-bucket_url=s3://mosaicml-internal-checkpoints-shared/sam/pile-of-law
+bucket_url="$1"
 
 # Upload the directory to the S3 bucket
 aws s3 cp $dir_path $bucket_url --recursive
