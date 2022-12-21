ab01431480bd3de44400e184d4f9d8a5e6e38bb4
Sam Havens <sam@mosaicml.com>
2022-12-21 00:44:43 +0000

    paranoid

--- tokenize/prepare_tokenize.py
+++ tokenize/prepare_tokenize.py
@@ -21,9 +21,10 @@ torchrun
 """
 
 import random
-from typing import List
+from typing import Any, List, Optional
 
 import spacy
+import streaming as ms
 
 try:
     spacy.load('en_core_web_sm')
@@ -52,3 +53,57 @@ def num_sents_from_len(doc: str) -> int:
         return 2
 
 
+class SentencesPileOfLaw(ms.StreamingDataset):
+    def __init__(self,
+                 nlp,
+                 local: str,
+                 remote: Optional[str] = None,
+                 split: Optional[str] = None,
+                 shuffle: bool = False,
+                 predownload: Optional[int] = 100_000,
+                 keep_zip: Optional[bool] = None,
+                 download_retry: int = 2,
+                 download_timeout: float = 60,
+                 validate_hash: Optional[str] = None,
+                 shuffle_seed: Optional[int] = None,
+                 num_canonical_nodes: Optional[int] = None,
+                 batch_size: Optional[int] = None) -> None:
+
+        super().__init__(local, remote, split, shuffle, predownload, keep_zip, download_retry,
+                         download_timeout, validate_hash, shuffle_seed, num_canonical_nodes,
+                         batch_size)
+        self.nlp = nlp
+
+    def __getitem__(self, idx: int) -> List[str]:
+        """Get sample by global index, blocking to load its shard if missing.
+        Args:
+            idx (int): Sample index.
+        Returns:
+            Any: Sample data.
+        """
+        text_sample = super().__getitem__(idx)
+        doc = self.nlp(text_sample['text'])
+        k = num_sents_from_len(doc.text)
+        sents = [s.text for s in doc.sents if s.text.strip() != '']
+        sents = get_sents(sents=sents, k=k)
+        return sents
+
+
+def main():
+    cfg = {
+        'name': 'pile_of_law',
+        'dataset': {
+            'remote': '../mds-pol',
+            'local': '../mds-pol',
+            'split': 'train',
+            'shuffle': True,
+            'prefetch': 1000,
+        },
+        'drop_last': False,
+        'num_workers': 8,
+        'pin_memory': True,
+        'prefetch_factor': 2,
+        'persistent_workers': True,
+        'timeout': 1200,
+    }
+    cfg = om.create(cfg)
